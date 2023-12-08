3f5dca39784068be38df93eba57093d5fd02e5fe
EliotTerrier <eliot.terrier@itxpt.org>
2023-12-08 14:36:13 +0100

    modification

--- itxpt_gnsslocation.py
+++ itxpt_gnsslocation.py
@@ -1,14 +1,14 @@
-import sys, socket, os, signal
+import socket, signal
 import threading
 import time
-import requests
+import Data
 import xml.etree.ElementTree as ET
 from datetime import datetime
 from service_discovery import publish_service
 
 MCAST_GRP = '239.255.42.21'
 MULTICAST_TTL = 2
-DATA_TEMPLATE_XML="GNSSLocationDelivery.xml"
+
 
 #gnsslocation service parameters 
 name = "ITxPT_gnsslocation"
@@ -22,8 +22,8 @@ properties = {
 }
 
 killService = False
-filename="/home/eliot/Documents/cloudlab/GNSSLocationDelivery.xml"
-xmlTree=""
+xml_data= Data.GNSSLocationDeliveryTemplate
+
 
 def publish_zeroconf_service():  
 	threading.Thread(target=publish_service, daemon=True, args=(name, service_type, ipaddress, port, properties)).start() # daemon=True to exit the subscrption process when the flask server is stopped
@@ -33,14 +33,15 @@ def exit_gracefully(signum,frame):
     global killService
     killService = True
 
-def changeXMLData(xml:ET.ElementTree,longitude,latitude):
-     xml.find("GNSSLocation/Data").text="" #empty data string 
-     currentdate=datetime.today()
-     xml.find("GNSSLocation/Latitude/Degree").text="48.877583"
-     xml.find("GNSSLocation/Longitude/Degree").text=f"{longitude:.6f}" #update longitude value 
-     xml.find("GNSSLocation/Time").text=currentdate.strftime("%H:%M:%S")
-     xml.find("GNSSLocation/Date").text=currentdate.strftime("%Y-%m-%d")
-     return ET.tostring(xml.getroot(),encoding="utf-8",xml_declaration=True) #converting the modified XML document back to a bytes-like object 
+def changeXMLData(xml_data, longitude, latitude):
+    xml = ET.fromstring(xml_data)  # Convert string to ElementTree object
+    xml.find(".//Data").text = ""  # Empty data string 
+    currentdate = datetime.today()
+    xml.find(".//Latitude/Degree").text = "48.877583"
+    xml.find(".//Longitude/Degree").text = f"{longitude:.6f}"  # Update longitude value 
+    xml.find(".//Time").text = currentdate.strftime("%H:%M:%S")
+    xml.find(".//Date").text = currentdate.strftime("%Y-%m-%d")
+    return ET.tostring(xml, encoding="utf-8", xml_declaration=True)  # Convert the modified ElementTree object back to a string
 
 
 def gnsslocation_daemon(ipaddress,port,xmlData):
@@ -52,7 +53,7 @@ def gnsslocation_daemon(ipaddress,port,xmlData):
 
 def background_job():
         global killService
-        global xmlTree
+        global xml_data
         global port
         global ipaddress
 
@@ -61,7 +62,7 @@ def background_job():
         longitude=2.338950
         while not killService:
                 time.sleep(1)
-                xmlData=changeXMLData(xmlTree,longitude,0)
+                xmlData=changeXMLData(xml_data,longitude,0)
                 longitude+=1E-6 #going east every second
                 print(xmlData)
                 gnss_thread = threading.Thread(target=gnsslocation_daemon, name='Thread-gnss_thread',kwargs={"ipaddress":ipaddress,"port":port,"xmlData":xmlData})
@@ -74,7 +75,7 @@ def background_job():
      
 # main 
 if __name__ == '__main__':
-    xmlTree = ET.parse(filename)
+
     signal.signal(signal.SIGINT, exit_gracefully)
     signal.signal(signal.SIGTERM, exit_gracefully)
     background_job()
